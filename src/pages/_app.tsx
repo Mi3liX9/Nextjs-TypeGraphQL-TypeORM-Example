07b0405d188f5020e25b9bbd79c453bab5b7ded7
Ali Al-Shaikh <mi3lix9@outlook.sa>
2020-09-22 22:17:06 +0300

    added styled components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
-// import "reflect-metadata";
 import { ApolloProvider } from "@apollo/client";
+import { AppProps } from "next/dist/next-server/lib/router/router";
 import { useApollo } from "src/lib/apollo-client";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: any) {
+function MyApp({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState);
 
   return (
