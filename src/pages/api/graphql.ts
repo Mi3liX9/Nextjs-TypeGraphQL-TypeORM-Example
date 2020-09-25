bee566fc3a310b1f95218fd6cb47f5b81d8dfdcc
Ali Al-Shaikh <mi3lix9@outlook.sa>
2020-09-25 18:49:55 +0300

    TEST

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -2,12 +2,22 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { options } from "src/lib/database-options";
 import { getApolloServerHandler } from "src/lib/type-graphql";
 import { connectDatabase } from "src/lib/typeORM";
+import NextConnect from "next-connect";
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  await connectDatabase(options);
-  const apolloServerHandler = await getApolloServerHandler();
-  return apolloServerHandler(req, res);
-};
+export default NextConnect<NextApiRequest, NextApiResponse>().use(
+  async (req, res, next) => {
+    await connectDatabase(options);
+    const apolloServerHandler = await getApolloServerHandler();
+    next();
+    return apolloServerHandler(req, res);
+  }
+);
+
+// export default async (req: NextApiRequest, res: NextApiResponse) => {
+//   await connectDatabase(options);
+//   const apolloServerHandler = await getApolloServerHandler();
+//   return apolloServerHandler(req, res);
+// };
 
 export const config = {
   api: {
